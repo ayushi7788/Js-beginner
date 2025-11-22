bf885008f7ad26556cb9048f18961014498d3cb2
Ayushi Tiwari <ayushit477@gmail.com>
2025-11-22 11:16:11 +0000

    02_datatypes done

---
+++ datatypes.js
@@ -0,0 +1,13 @@
+"use strict"; //treat all JS code as newer version
+//alert(3+3) // we are using node js, not browser
+let name = "Ayushi" //string
+let age = 18 //numbers
+let isLoggedIn = false //boolean
+//bigint => large numbers
+//null => standalone value
+//undefined
+//symbol => unique
+
+//object
+
+console.log(typeof age);
