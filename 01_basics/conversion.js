7766fb756c3a3bd2e0720a5fc096ee29879fc564
Ayushi Tiwari <ayushit477@gmail.com>
2025-11-26 14:18:34 +0000

    Conversion done

--- 01_basics/conversion.js
+++ 01_basics/conversion.js
@@ -1,7 +1,15 @@
-let score = "33"
+let score = "33abc"
 
 console.log(typeof score);
 console.log(typeof (score));
 
 let valueInNumber = Number(score)
 console.log((typeof valueInNumber));
+console.log((valueInNumber));
+
+//string => NaN
+//undefined => NaN
+// null => object 
+// true => 1; false => 0
+//"" => false
+
